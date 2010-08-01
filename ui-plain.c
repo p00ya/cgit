887f3ea3110fd44af18addbff11130c156f77c1f
Mark Lodato <lodatom@gmail.com>
2010-08-01 16:11:30 -0400

    ui-plain: add support for symbolic links
    Follow symbolic links in the plain view.  A new tree-walking function
    is needed, since read_tree_recursive() does not have an option to
    dereference symbolic links within the directory portion of a match.
    The new walk_tree() function operates on a single path and returns the
    sha1 and mode of the resulting file.
    
    Since a callback function is no longer needed, consolodate the
    print_dir() family into a single function.
    
    Write a simple normalize_path() to remove '.' and '' elements from
    a path.  The existing normalize_path_copy() is not appropriate here,
    since that function does not follow symbolic links.  For example, if
    'x' links to directory 'a/b', then 'x/..' should return 'a', not ''.
    
    Signed-off-by: Mark Lodato <lodatom@gmail.com>

--- ui-plain.c
+++ ui-plain.c
@@ -10,8 +10,8 @@
 #include "html.h"
 #include "ui-shared.h"
 
-int match_baselen;
-int match;
+#define MAX_SYMLINKS 10
+#define MAX_DEPTH 128
 
 __attribute__((format (printf,1,2)))
 static void not_found(const char *format, ...);
@@ -64,10 +64,12 @@ static int ensure_slash()
 static void print_object(const unsigned char *sha1, const char *path)
 {
 	enum object_type type;
-	char *buf, *ext;
+	char *buf, *ext, *slash;
 	unsigned long size;
 	struct string_list_item *mime;
 
+	while ((slash = strchr(path, '/')))
+		path = slash + 1;
 	type = sha1_object_info(sha1, &size);
 	if (type == OBJ_BAD) {
 		not_found("Bad object: %s", sha1_to_hex(sha1));
@@ -97,111 +99,260 @@ static void print_object(const unsigned char *sha1, const char *path)
 	ctx.page.etag = sha1_to_hex(sha1);
 	cgit_print_http_headers(&ctx);
 	html_raw(buf, size);
-	match = 1;
 }
 
-static int print_dir(const unsigned char *sha1, const char *path,
-		      const char *base)
+static void print_dir(const unsigned char *sha1, const char *path)
 {
-	char *fullpath;
+	struct tree_desc desc;
+	struct name_entry entry;
+	struct tree *tree;
+
 	if (!ensure_slash())
-		return 0;
-	if (path[0] || base[0])
-		fullpath = fmt("/%s%s/", base, path);
+		return;
+
+	if (path[0])
+		path = fmt("/%s/", path);
 	else
-		fullpath = "/";
+		path = "/";
+
+	tree = lookup_tree(sha1);
+	if (!tree || parse_tree(tree)) {
+		not_found("Invalid tree");
+		return;
+	}
+
 	ctx.page.etag = sha1_to_hex(sha1);
 	cgit_print_http_headers(&ctx);
 	html("<html><head><title>");
-	html_txt(fullpath);
+	html_txt(path);
 	html("</title></head>\n<body>\n <h2>");
-	html_txt(fullpath);
+	html_txt(path);
 	html("</h2>\n <ul>\n");
-	if (path[0] || base[0])
+	if (path[1])
 	      html("  <li><a href=\"../\">../</a></li>\n");
-	match = 2;
-	return 1;
-}
 
-static void print_dir_entry(const unsigned char *sha1, const char *path,
-			    unsigned mode)
-{
-	char *url;
-	if (S_ISDIR(mode))
-		url = fmt("%s/", path);
-	else
-		url = fmt("%s", path);
-	html("  <li><a href='");
-	html_url_path(url);
-	html("'>");
-	html_txt(url);
-	html("</a></li>\n");
-	match = 2;
-}
+	init_tree_desc(&desc, tree->buffer, tree->size);
+	while (tree_entry(&desc, &entry)) {
+		char *url;
+		if (S_ISDIR(entry.mode))
+			url = fmt("%s/", entry.path);
+		else
+			url = fmt("%s", entry.path);
+		html("  <li><a href='");
+		html_url_path(url);
+		html("'>");
+		html_txt(url);
+		html("</a></li>\n");
+	}
 
-static void print_dir_tail(void)
-{
 	html(" </ul>\n</body></html>\n");
 }
 
-static int walk_tree(const unsigned char *sha1, const char *base, int baselen,
-		     const char *pathname, unsigned mode, int stage,
-		     void *cbdata)
+static int follow_symlink(const unsigned char *sha1, struct strbuf *path,
+			  int start, const char *pathname)
 {
-	if (baselen == match_baselen) {
-		if (S_ISREG(mode))
-			print_object(sha1, pathname);
-		else if (S_ISDIR(mode)) {
-			if (print_dir(sha1, pathname, base))
-				return READ_TREE_RECURSIVE;
-		}
+	enum object_type type;
+	unsigned long size, pathlen;
+	char *value, *slash;
+
+	type = sha1_object_info(sha1, &size);
+	if (type == OBJ_BAD) {
+		not_found("Bad object: %s", sha1_to_hex(sha1));
+		return -1;
 	}
-	else if (baselen > match_baselen)
-		print_dir_entry(sha1, pathname, mode);
-	else if (S_ISDIR(mode))
-		return READ_TREE_RECURSIVE;
+
+	value = read_sha1_file(sha1, &type, &size);
+	if (!value) {
+		not_found("Object not found: %s", sha1_to_hex(sha1));
+		return -1;
+	}
+	if (size == 0) {
+		not_found("Zero-length symbolic link");
+		return -1;
+	}
+	if (value[0] == '/') {
+		not_found("Absolute symbolic link: %s -> %s", pathname, value);
+		return -1;
+	}
+
+	slash = strchr(path->buf + start, '/');
+	if (slash)
+		pathlen = slash - (path->buf + start);
+	else
+		pathlen = strlen(path->buf + start);
+	strbuf_splice(path, start, pathlen, value, size);
 
 	return 0;
 }
 
-static int basedir_len(const char *path)
+/* Remove "" and "." path components.
+ * Leave (single) trailing slash if it exists.
+ * 'dst' and 'src' may overlap, as long as dst <= src. */
+static void normalize_path(char *dst, const char *src)
 {
-	char *p = strrchr(path, '/');
-	if (p)
-		return p - path + 1;
-	return 0;
+	while (*src == '/')
+		src++;
+	while (*src) {
+		char *slash = strchr(src, '/');
+		size_t len;
+		if (slash)
+			len = slash - src;
+		else
+			len = strlen(src);
+		if (!((len == 0) ||
+		      (len == 1 && src[0] == '.') ||
+		      (len == 1 && src[0] == '.' && src[1] == '.')))
+		{
+			memmove(dst, src, len);
+			dst += len;
+			if (slash)
+				*dst++ = '/';
+		}
+		src += len;
+		while (*src == '/')
+			src++;
+	}
+	*dst++ = '\0';
+}
+
+/* return 1 if the first path component of 'path' begins with 'prefix' */
+static int path_prefix(const char *path, const char *prefix)
+{
+	char *slash = strchr(path, '/');
+	if (slash) {
+		size_t len = slash - path;
+		size_t prefix_len = strlen(prefix);
+		return (len == prefix_len) && !strncmp(path, prefix, len);
+	} else
+		return !strcmp(path, prefix);
+}
+
+static int walk_tree(const unsigned char *sha1_root, struct strbuf *pathbuf,
+		     unsigned char *sha1_out, int maxlinks)
+{
+	static struct tree *trees[MAX_DEPTH];
+	static struct name_entry entries[MAX_DEPTH];
+	char *p;
+	int depth;
+
+	if (maxlinks <= 0) {
+		not_found("Too many symbolic links");
+		return -1;
+	}
+
+	memset(trees, 0, sizeof trees);
+	trees[0] = parse_tree_indirect(sha1_root);
+	if (!trees[0]) {
+		not_found("Invalid sha1: %s", sha1_to_hex(sha1_root));
+		return -1;
+	}
+	entries[0].sha1 = trees[0]->object.sha1;
+	entries[0].path = "";
+	entries[0].mode = S_IFDIR;
+
+	normalize_path(pathbuf->buf, pathbuf->buf);
+	strbuf_setlen(pathbuf, strlen(pathbuf->buf));
+
+	p = pathbuf->buf;
+	if (!strcmp(p, "") || !strcmp(p, "/")) {
+		memcpy(sha1_out, entries[0].sha1, 20);
+		return entries[0].mode;
+	}
+	for (depth = 0; depth < MAX_DEPTH-1; depth++) {
+		struct tree *tree;
+		struct tree_desc desc;
+		struct name_entry *entry;
+		int found;
+		char *slash;
+
+
+		while (path_prefix(p, "..")) {
+			if (depth <= 0) {
+				not_found("Symbolic link outside top level: %s",
+					  pathbuf->buf);
+				return -1;
+			}
+			trees[depth] = NULL;
+			depth--;
+			if (p[2] == '\0' || (p[2] == '/' && p[3] == '\0')) {
+				memcpy(sha1_out, entries[depth].sha1, 20);
+				return entries[depth].mode;
+			}
+			p += 3;
+		}
+
+		tree = trees[depth];
+		if (!tree) {
+			tree = lookup_tree(entries[depth].sha1);
+			if (!tree || parse_tree(tree)) {
+				not_found("Invalid tree");
+				return -1;
+			}
+			trees[depth] = tree;
+		}
+		init_tree_desc(&desc, tree->buffer, tree->size);
+
+
+		entry = &entries[depth+1];
+		found = 0;
+		while (tree_entry(&desc, entry)) {
+			if (path_prefix(p, entry->path)) {
+				found = 1;
+				break;
+			}
+		}
+
+		slash = strchr(p, '/');
+		if (!found) {
+			not_found("File not found");
+			return -1;
+		} else if (S_ISLNK(entry->mode)) {
+			if (follow_symlink(entry->sha1, pathbuf,
+					   p-pathbuf->buf, entry->path))
+				return -1;
+			return walk_tree(sha1_root, pathbuf, sha1_out, maxlinks-1);
+		} else if (!slash) {
+			memcpy(sha1_out, entry->sha1, 20);
+			return entry->mode;
+		} else if (!S_ISDIR(entry->mode)) {
+			not_found("Not a directory: %s",
+				  pathbuf->buf);
+			return -1;
+		} else
+			p = slash + 1;
+	}
+
+	not_found("Directory nesting too deep");
+	return -1;
 }
 
 void cgit_print_plain(struct cgit_context *ctx)
 {
 	const char *rev = ctx->qry.sha1;
-	unsigned char sha1[20];
-	struct commit *commit;
-	const char *paths[] = {ctx->qry.path, NULL};
+	struct strbuf pathbuf = STRBUF_INIT;
+	unsigned char sha1_rev[20], sha1_object[20];
+	char *pathname;
+	int mode;
 
 	if (!rev)
 		rev = ctx->qry.head;
-
-	if (get_sha1(rev, sha1)) {
+	if (get_sha1(rev, sha1_rev)) {
 		not_found("Ref not found: %s", rev);
 		return;
 	}
-	commit = lookup_commit_reference(sha1);
-	if (!commit || parse_commit(commit)) {
-		not_found("Invalid commit sha1: %s", sha1_to_hex(sha1));
-		return;
-	}
-	if (!paths[0]) {
-		paths[0] = "";
-		match_baselen = -1;
-		if (!print_dir(commit->tree->object.sha1, "", ""))
-			return;
-	}
-	else
-		match_baselen = basedir_len(paths[0]);
-	read_tree_recursive(commit->tree, "", 0, 0, paths, walk_tree, NULL);
-	if (!match)
-		not_found("File not found");
-	else if (match == 2)
-		print_dir_tail();
+
+	if (ctx->qry.path)
+		strbuf_addstr(&pathbuf, ctx->qry.path);
+	pathname = xstrdup(pathbuf.buf);
+
+	mode = walk_tree(sha1_rev, &pathbuf, sha1_object, MAX_SYMLINKS);
+	if (S_ISDIR(mode))
+		print_dir(sha1_object, pathname);
+	else if (S_ISREG(mode))
+		print_object(sha1_object, pathname);
+	else if (mode >= 0)
+		not_found("Invalid mode: %d", mode);
+
+	strbuf_release(&pathbuf);
+	free(pathname);
 }
